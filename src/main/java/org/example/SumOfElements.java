0c242acecf9b294b24fd3cd04430ea87d6db7173
Welcome <sayaliadgale@gmail.com>
2024-07-10 10:05:00 +0530

    Find Sum Of All Elements

---
+++ src/main/java/org/example/SumOfElements.java
@@ -0,0 +1,17 @@
+package org.example;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SumOfElements {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(1,2,3,4,5);
+
+        int sum = nums.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+        System.out.println("Sum " + sum);
+
+    }
+}
